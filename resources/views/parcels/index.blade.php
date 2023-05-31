510017bec16f75ba93f965fabfdcd34717aab61b
Dima Bielov <97897846+Mandalayyy@users.noreply.github.com>
2023-05-31 14:06:04 +0300

    add modal

--- resources/views/parcels/index.blade.php
+++ resources/views/parcels/index.blade.php
@@ -23,30 +23,38 @@
                 <td>{{ $parcel->description }}</td>
                 <td>
                     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
-                        Open Modal
+                        Переглянути
                     </button>
+                    <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Детальніше</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                <p><strong>Sender:</strong> {{ $parcel->sender }}</p>
+                                <p><strong>Reciever:</strong> {{ $parcel->receiver }}</p>
+                                <p><strong>Description:</strong> {{ $parcel->description }}</p>
+                                <p><strong>From Address:</strong> {{ $parcel->from_address }}</p>
+                                <p><strong>To Address:</strong> {{ $parcel->to_address }}</p>
+                                <p><strong>Location:</strong> {{ $parcel->location }}</p>
+                                <p><strong>Created at:</strong> {{ $parcel->created_at }}</p>
+                                <p><strong>Updated at:</strong> {{ $parcel->updated_at }}</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                    </div>
+                </div>
+            </div>
                 </td>
             </tr>
 
             <!-- Modal -->
-            <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal Title</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <p>This is the modal body.</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
             @endforeach
+            
         </tbody>
     </table>
 
